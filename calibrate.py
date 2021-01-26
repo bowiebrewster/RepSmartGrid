7a3bffc54198f8b109787236e21c6c214c01c44c
xemmiie <emmelienschiet15@gmail.com>
2021-01-26 22:53:22 +0100

    testinggg

--- calibrate.py
+++ calibrate.py
@@ -2,34 +2,45 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-import random
-import copy
+from code.algorithms import simulatedannealing as sa
+from code.algorithms import random as r
+from code.classes.district import District
+# import random
+# import copy
 import numpy as np
-import operator
-import itertools
-import collections
-import math
+# import operator
+# import itertools
+# import collections
+# import math
 
-    xx = np.linspace(0.01, 0.05, num=100) # cooling rate
-    yy = np.linspace(20, 40, num=100) # Temperature
-    X, Y = np.meshgrid(xx, yy)
-    Z = []
-    for Xrow, Yrow in zip(X, Y):
-        z = []
-        for cr, start_temp in zip(Xrow, Yrow):
-            z.append(sa.calibrate(cr, start_temp))
-        Z.append(z)
-        if len(Z) % 10 == 0:
-            print(f'Hoppakee, 10% erbij bro')
-  
-    Z = np.array(Z)
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0)
-    i, j  = np.unravel_index(Z.argmin(), Z.shape())
-    print(f'Best i = {i} - Best j = {j}')
-    print(Z[i][j])
-    print(f'{time.time() - start}')
-    plt.show()
+district = District(2)
+algorithm = r.Random(district)
+algorithm.run(False, False)
+calib = sa.SimulatedAnnealing(algorithm)
+
+xx = np.linspace(0.01, 0.05, num=100) # cooling rate
+yy = np.linspace(20, 40, num=100) # temperature
+X, Y = np.meshgrid(xx, yy)
+Z = []
+for Xrow, Yrow in zip(X, Y):
+    z = []
+    for cr, start_temp in zip(Xrow, Yrow):
+        print(f"cooling rate {cr} and start temp {start_temp}")
+        cost = calib.calibrate(cr, start_temp)
+        z.append(cost)
+    Z.append(z)
+    if len(Z) % 10 == 0:
+        print(f'Hoppakee, 10% erbij bro')
+
+Z = np.array(Z)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0)
+loc = np.argwhere(Z == np.min(Z)[0])
+i, j = loc[0], loc[1]
+best_cr = xx[i]
+best_T = yy[j]
+print(f"Best cooling rate: {best_cr} and Best temperature: {best_T}")
+print(f"{Z[i][j]}")
+plt.show()
